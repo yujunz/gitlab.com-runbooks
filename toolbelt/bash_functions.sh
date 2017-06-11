3ffe9180eb18ea74c050014c1c7611336b3c2baf
Pablo Carranza <pcarranza@gmail.com>
2017-06-11 12:35:55 +0200

    Add safeguards to prevent setting an invalid environment
    If the required environment variables are not set the functions script
    will fail. In the same spirit, the default values will not be set if
    the folder or files do not exists.
    
    This is so to prevent having a partially set environment.
    
    Additionally, all the functions that fail will return 1 to set the right
    return code.

--- toolbelt/bash_functions.sh
+++ toolbelt/bash_functions.sh
@@ -6,6 +6,11 @@
 # GITLAB_SSH_USER
 # GITLAB_SSH_KEY
 
+[ -z "${GITLAB_CHEF_REPO_DIR}" ] && echo "GITLAB_CHEF_REPO_DIR is not defined" && return 1
+[ -z "${GITLAB_COOKBOOKS_DIR}" ] && echo "GITLAB_COOKBOOKS_DIR is not defined" && return 1
+[ -z "${GITLAB_SSH_USER}" ] && echo "GITLAB_SSH_USER is not defined" && return 1
+[ -z "${GITLAB_SSH_KEY}" ] && echo "GITLAB_SSH_KEY is not defined" && return 1
+
 ## Terraform
 function gitlab-terraform {
   case $1 in
@@ -51,13 +56,13 @@ function cookbook {
 
 function gitlab-nodes {
   pushd . >/dev/null
-  cd ${GITLAB_CHEF_REPO_DIR} || return
+  cd ${GITLAB_CHEF_REPO_DIR} || return 1
   knife node list
   popd >/dev/null
 }
 
 function gip {
-  [ -z $1 ] && echo "No hostname provided" && return
+  [ -z $1 ] && echo "No hostname provided" && return 1
   pushd . >/dev/null
   cd ${GITLAB_CHEF_REPO_DIR} || return
   knife node show $1 | awk '/^IP/{print $2}'
@@ -68,12 +73,12 @@ function gssh {
   ip=$1
   shift
   [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ip=$(gip $ip)
-  [ -z $ip ] && echo "Invalid ip or hostname" && return
+  [ -z $ip ] && echo "Invalid ip or hostname" && return 1
   ssh -i ${GITLAB_SSH_KEY} ${GITLAB_SSH_USER}@$ip $@
 }
 
 function ghaproxy-stats-tunnel {
-  [ -z $1 ] && echo "No hostname provided" && return
+  [ -z $1 ] && echo "No hostname provided" && return 1
   gssh $1 -nNTL 7331:127.0.0.1:7331 -p 2222
 }
 
@@ -90,7 +95,7 @@ function gitlab-nodes-with-ips {
 function rm_merged_branches {
   if [[ ! -d .git ]]; then
     echo "Not a git repo" 
-    return
+    return 1
   fi
   for branch in `git branch --merged | grep -v master`
   do 
--- toolbelt/bash_gitlab.sh
+++ toolbelt/bash_gitlab.sh
@@ -4,12 +4,12 @@
 local DIR=$(dirname $0)
 for ENV_FILE in $(ls "${DIR}/.env"); do
   source "${DIR}/.env/${ENV_FILE}"
-end
+done
 
-[ -z "${GITLAB_CHEF_REPO_DIR}" ] && export GITLAB_CHEF_REPO_DIR="${HOME}/Projects/gitlab/chef-repo"
-[ -z "${GITLAB_COOKBOOKS_DIR}" ] && export GITLAB_COOKBOOKS_DIR="${HOME}/Projects/gitlab/cookbooks"
+[ -z "${GITLAB_CHEF_REPO_DIR}" ] && [ -d "${HOME}/Projects/gitlab/chef-repo" ] && export GITLAB_CHEF_REPO_DIR="${HOME}/Projects/gitlab/chef-repo"
+[ -z "${GITLAB_COOKBOOKS_DIR}" ] && [ -d "${HOME}/Projects/gitlab/cookbooks" ] && export GITLAB_COOKBOOKS_DIR="${HOME}/Projects/gitlab/cookbooks"
+[ -z "${GITLAB_SSH_KEY}" ]       && [ -f "${HOME}/.ssh/id_rsa-gitlab" ] && export GITLAB_SSH_KEY="${HOME}/.ssh/id_rsa-gitlab"
 [ -z "${GITLAB_SSH_USER}" ]      && export GITLAB_SSH_USER="$(whoami)"
-[ -z "${GITLAB_SSH_KEY}" ]       && export GITLAB_SSH_KEY="${HOME}/.ssh/id_rsa-gitlab"
 
 source "${DIR}/bash_aliases.sh"
 source "${DIR}/bash_functions.sh"
