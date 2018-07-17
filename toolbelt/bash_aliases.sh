24c8bfbedd6c0f51db5d26e2009d0c6c0d6dbdc7
John T Skarbek <jtslear@gmail.com>
2018-07-17 13:24:04 -0400

    It was recommended that I not use this
    * So let's remove stuff that we don't use from our codebase
    
    Signed-off-by: John T Skarbek <jtslear@gmail.com>

--- toolbelt/bash_aliases.sh
+++
@@ -1,6 +0,0 @@
-# GitLab Aliases
-alias   knife="bundle exec knife"
-alias    rake="bundle exec rake"
-alias kitchen="bundle exec kitchen"
-alias   berks="bundle exec berks"
-[ -z "$GITLAB_SSH_KEY" ] && alias kssh="knife ssh -i $GITLAB_SSH_KEY"
