94122ff864350813cdbccc45c74ca543b23e6197
Andrew Newdigate <andrew@gitlab.com>
2018-09-04 07:35:32 +0000

    Abuse alerting

---
+++ elastic-watcher/update-alerts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+for i in "${SCRIPT_DIR}"/watches/*.json; do
+  base_name=$(basename "$i")
+  name=${base_name%.json}
+  curl --retry 3 --silent --fail -X PUT "${ES_URL}/_xpack/watcher/watch/${name}?pretty=true" -H 'Content-Type: application/json'  --data-binary "@${i}"
+done
