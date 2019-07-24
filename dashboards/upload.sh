c7e37d034eba8b644c35bca6e5b8275b5563a819
John Jarvis <jarv@gitlab.com>
2019-07-24 18:45:25 +0000

    Adds dry-run mode to dashboard script

--- dashboards/upload.sh
+++ dashboards/upload.sh
@@ -1,7 +1,51 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
 IFS=$'\n\t'
+
+usage() {
+    cat <<-EOF
+    Usage $0 [Dh]
+
+    DESCRIPTION
+        This script generates dashboards and uploads
+        them to dashboards.gitlab.net
+
+        GRAFANA_API_TOKEN must be set in the environment
+
+    FLAGS
+        -D  run in Dry-run
+        -h  help
+
+	EOF
+}
+
+
+while getopts ":Dh" o; do
+    case "${o}" in
+        D)
+            dry_run="true"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+
+dry_run=${dry_run:-}
+
+if [[ -z $dry_run && -z ${GRAFANA_API_TOKEN:-} ]]; then
+    echo "You must set GRAFANA_API_TOKEN to use this script, or run in dry run mode"
+    usage
+    exit 1
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -23,16 +67,9 @@ find_dashboards "$@"|while read -r line; do
 
   uid="${folder}-$(basename "$line"|sed -e 's/\..*//')"
 
-  # Note: create folders with `create-grafana-folder.sh` to configure the UID
-  folderId=$(curl --silent --fail \
-    -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    "https://dashboards.gitlab.net/api/folders/${folder}" | jq '.id')
-
   extension="${relative##*.}"
   if [[ "$extension" == "jsonnet" ]]; then
-    dashboard=$(jsonnet -J "${SCRIPT_DIR}" -J "${SCRIPT_DIR}/vendor" "${line}")
+    dashboard=$(jsonnet -J "${SCRIPT_DIR}" -J "${SCRIPT_DIR}/vendor" -J "${SCRIPT_DIR}/vendor/grafonnet-lib" "${line}")
   else
     dashboard=$(cat "${line}")
   fi
@@ -42,6 +79,19 @@ find_dashboards "$@"|while read -r line; do
     # If the dashboard doesn't have a uid, configure one
     dashboard=$(echo "${dashboard}" | jq ".uid = \"$uid\"")
   fi
+
+  if [[ -n $dry_run ]]; then
+      echo "Running in dry run mode, would create $line in folder $folder with uid $uid"
+      continue
+  fi
+
+  # Note: create folders with `create-grafana-folder.sh` to configure the UID
+  folderId=$(curl --silent --fail \
+    -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    "https://dashboards.gitlab.net/api/folders/${folder}" | jq '.id')
+
 
   response=$(curl --silent --fail \
     -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
