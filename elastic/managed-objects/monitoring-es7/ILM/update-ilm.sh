378796d67b8a8e292790b48312e2b959c538b598
Michal Wasilewski <mwasilewski@gitlab.com>
2020-01-07 10:52:14 +0000

    adjust watcher ilm policy
    Signed-off-by: Michal Wasilewski <mwasilewski@gitlab.com>

---
+++ elastic/managed-objects/monitoring-es7/ILM/update-ilm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+export ES_URL=$ES_MONITORING_ES7_URL
+source "${SCRIPT_DIR}"/../../lib/update-scripts-functions.sh
+
+ES7_ILM_exec_jsonnet_and_upload_json
