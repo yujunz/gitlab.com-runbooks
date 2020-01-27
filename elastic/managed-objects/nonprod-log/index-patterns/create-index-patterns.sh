af7d596d8bcc115c8233a6494c2640db4556d27b
Michal Wasilewski <mwasilewski@gitlab.com>
2020-01-27 14:26:19 +0100

    remove index-patterns related automation
    Signed-off-by: Michal Wasilewski <mwasilewski@gitlab.com>

--- elastic/managed-objects/nonprod-log/index-patterns/create-index-patterns.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-export KIBANA_URL=$KIBANA_NONPROD_URL
-source "${SCRIPT_DIR}"/../../lib/update-scripts-functions.sh
-
-kibana_post_json "api/saved_objects/index-pattern/"
