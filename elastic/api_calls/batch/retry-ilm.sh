905d6fd2b6899f4a83a7d7c0b6523f50a1854da1
Igor Wiedler <iwiedler@gitlab.com>
2020-05-07 17:42:07 +0200

    shellcheck

--- elastic/api_calls/batch/retry-ilm.sh
+++ elastic/api_calls/batch/retry-ilm.sh
@@ -7,7 +7,8 @@ if ! curl -s "${ELASTICSEARCH_URL}" > /dev/null; then
   exit 1
 fi
 
-while [[ "$(curl -s "${ELASTICSEARCH_URL}/*/_ilm/explain?only_errors" | jq -r '.indices|length')" > 0 ]]; do
+while [[ "$(curl -s "${ELASTICSEARCH_URL}/*/_ilm/explain?only_errors" | jq -r '.indices|length')" -gt 0 ]]; do
+  # shellcheck disable=SC2016
   curl -s "${ELASTICSEARCH_URL}/*/_ilm/explain?only_errors" | jq -r '.indices|keys[]' | xargs -n1 bash -c 'echo $0; curl -s -XPOST "${ELASTICSEARCH_URL}/$0/_ilm/retry"; echo; sleep 10'
   echo
 done
