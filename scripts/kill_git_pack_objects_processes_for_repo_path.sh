35b2fe61bfa7c8dd7e810aa314b80f4a979d74e1
cmcfarland <cmcfarland@gitlab.com>
2020-09-08 13:15:39 -0400

    Script updates

--- scripts/kill_git_pack_objects_processes_for_repo_path.sh
+++ scripts/kill_git_pack_objects_processes_for_repo_path.sh
@@ -6,22 +6,24 @@ TARGET_GIT_DIR=$1
 for PACK_PID in $( pgrep -f 'git pack-objects .*--stdout' )
 do
     echo "Examining PID $PACK_PID"
-    ps uwf -p $PACK_PID | cat
+    ps uwf -p "$PACK_PID" | cat
 
-    PARENT_PID=$( ps -p $PACK_PID -o ppid= )
-    ps -p $PARENT_PID -o args= | grep -q 'git .*upload-pack' || continue
+    PARENT_PID=$( ps -p "$PACK_PID" -o ppid= )
+    # shellcheck disable=SC2009
+    ps -p "$PARENT_PID" -o args= | grep -q 'git .*upload-pack' || continue
 
     echo "This PID is a child of upload-pack:"
-    ps uwf -p $PARENT_PID | cat
+    ps uwf -p "$PARENT_PID" | cat
 
     echo "Checking repo dir"
-    PID_CURRENT_DIR=$( sudo ls -l /proc/$PACK_PID/cwd | perl -pe 's/.* -> //' )
+    PID_CURRENT_DIR=$( sudo ls -l "/proc/$PACK_PID/cwd" | perl -pe 's/.* -> //' )
     echo "$PID_CURRENT_DIR" | grep -q "$TARGET_GIT_DIR" || continue
     echo "PID current dir matches target git dir"
 
     echo "Killing git-pack-objects pid: $PACK_PID"
     (
-        echo "Killing pid $PACK_PID: $( ps uwf -p $PACK_PID | grep -v 'PID' )"
-    ) 1>&2 
-    sudo kill $PACK_PID
+        # shellcheck disable=SC2009
+        echo "Killing pid $PACK_PID: $( ps uwf -p "$PACK_PID" | grep -v 'PID' )"
+    ) 1>&2
+    sudo kill "$PACK_PID"
 done
