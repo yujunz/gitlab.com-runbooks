7de5b4c98830383adb0185bc363ab39391911590
Igor Wiedler <iwiedler@gitlab.com>
2020-10-29 16:53:56 +0100

    shfmt

--- elastic/scheduled/hot_index_shards_per_node.sh
+++ elastic/scheduled/hot_index_shards_per_node.sh
@@ -16,13 +16,13 @@ set -euo pipefail
 # -n option will not actually apply the settings
 dry_run=0
 while getopts 'n' opt; do
-	case "$opt" in
-	n) dry_run=1 ;;
-	*)
-		echo 'error in command line parsing' >&2
-		exit 1
-		;;
-	esac
+  case "$opt" in
+    n) dry_run=1 ;;
+    *)
+      echo 'error in command line parsing' >&2
+      exit 1
+      ;;
+  esac
 done
 
 INDEX_MATCH_DATA="${INDEX_MATCH_DATA:-hot}"
@@ -34,32 +34,32 @@ TMPDIR="$(mktemp -d)"
 
 # fetch indices that are being written to
 curl -s "$ELASTICSEARCH_URL/_aliases" |
-	jq -r 'to_entries[]|select(.value.aliases[].is_write_index)|.key' |
-	sort >"${TMPDIR}/aliases.txt"
+  jq -r 'to_entries[]|select(.value.aliases[].is_write_index)|.key' |
+  sort >"${TMPDIR}/aliases.txt"
 
 # match indices
 curl -s "$ELASTICSEARCH_URL/*/_settings" |
-	jq -r --arg INDEX_MATCH_DATA "$INDEX_MATCH_DATA" \
-		--arg INDEX_MATCH_NUMBER_OF_SHARDS "$INDEX_MATCH_NUMBER_OF_SHARDS" \
-		--arg INDEX_MATCH_TOTAL_SHARDS_PER_NODE "$INDEX_MATCH_TOTAL_SHARDS_PER_NODE" \
-		'to_entries[]|select(.value.settings.index.routing.allocation.require.data == $INDEX_MATCH_DATA and .value.settings.index.number_of_shards == $INDEX_MATCH_NUMBER_OF_SHARDS and .value.settings.index.routing.allocation.total_shards_per_node == $INDEX_MATCH_TOTAL_SHARDS_PER_NODE)|.key' |
-	sort >"${TMPDIR}/indices.txt"
+  jq -r --arg INDEX_MATCH_DATA "$INDEX_MATCH_DATA" \
+    --arg INDEX_MATCH_NUMBER_OF_SHARDS "$INDEX_MATCH_NUMBER_OF_SHARDS" \
+    --arg INDEX_MATCH_TOTAL_SHARDS_PER_NODE "$INDEX_MATCH_TOTAL_SHARDS_PER_NODE" \
+    'to_entries[]|select(.value.settings.index.routing.allocation.require.data == $INDEX_MATCH_DATA and .value.settings.index.number_of_shards == $INDEX_MATCH_NUMBER_OF_SHARDS and .value.settings.index.routing.allocation.total_shards_per_node == $INDEX_MATCH_TOTAL_SHARDS_PER_NODE)|.key' |
+  sort >"${TMPDIR}/indices.txt"
 
 # remove indices that are still being written to
 grep -v -x -f "${TMPDIR}/aliases.txt" "${TMPDIR}/indices.txt" >"${TMPDIR}/target_indices.txt" || true
 
 # prepare settings to be applied
 echo '{}' |
-	jq -c --arg INDEX_APPLY_TOTAL_SHARDS_PER_NODE "$INDEX_APPLY_TOTAL_SHARDS_PER_NODE" \
-		'{"index.routing.allocation.total_shards_per_node": $INDEX_APPLY_TOTAL_SHARDS_PER_NODE}' \
-		>"${TMPDIR}/settings.txt"
+  jq -c --arg INDEX_APPLY_TOTAL_SHARDS_PER_NODE "$INDEX_APPLY_TOTAL_SHARDS_PER_NODE" \
+    '{"index.routing.allocation.total_shards_per_node": $INDEX_APPLY_TOTAL_SHARDS_PER_NODE}' \
+    >"${TMPDIR}/settings.txt"
 
 # apply settings to all indices
 while IFS= read -r index; do
-	echo "applying ${index}"
-	if [[ "$dry_run" -eq 0 ]]; then
-		curl -XPUT "$ELASTICSEARCH_URL/${index}/_settings" -d "$(cat "${TMPDIR}/settings.txt")"
-	fi
+  echo "applying ${index}"
+  if [[ "$dry_run" -eq 0 ]]; then
+    curl -XPUT "$ELASTICSEARCH_URL/${index}/_settings" -d "$(cat "${TMPDIR}/settings.txt")"
+  fi
 done <"${TMPDIR}/target_indices.txt"
 
 rm -rf "$TMPDIR"
